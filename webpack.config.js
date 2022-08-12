f7b796d4458de419a3e95b8269b88e1e9895aacb
shubhali503 <shubhaliagarwal555@gmail.com>
2022-08-12 19:35:24 +0530

    commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+// const WebpackDevServer = require('webpack-dev-server');
+
+module.exports = {
+    mode: "development",
+    devtool: "source-map",
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        // main: "./sass/styles.scss",
+        index: "./js/index.js"
+    },
+    output : {
+        path : path.resolve(__dirname, 'dist'),
+        filename : "bundle.js"
+    },
+     
+    module: {
+      rules: [
+        {
+          test: /\.(scss|css)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+                loader: miniCss.loader,
+                // options: {
+                //     reloadAll: true
+                // }
+            },
+            "css-loader",
+            "postcss-loader",
+            "sass-loader",
+          ]
+        }
+      ]
+    },
+    plugins: [
+        new miniCss({
+            filename: '[name].css'
+        }),
+        new htmlWebpackPlugin({
+            template: "./index.html"
+        }),
+    ],
+    devServer : {
+        static : {
+            directory: path.join(__dirname, 'dist')
+        },
+        port : 3000,
+    }
+  };
